54201d3ec5830895b0dd0572893ca00660b9090b
Jonathan Cui <jonathan.cui@hulu.com>
2017-03-15 18:03:55 -0700

    Update history config. Move it to a separate file.

---
+++ zsh/history.zsh
@@ -0,0 +1,19 @@
+# history
+if [ -z "$HISTFILE" ]; then
+    HISTFILE=$HOME/.zsh_history
+fi
+
+HISTSIZE=10000
+SAVEHIST=10000
+
+setopt APPEND_HISTORY
+setopt EXTENDED_HISTORY
+# for sharing history between zsh processes
+setopt HIST_VERIFY
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
+# ignore duplicate history
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
+
--- zshrc
+++ zshrc
@@ -21,14 +21,6 @@ BASE16_SHELL="$HOME/dotfiles/color-schemes/shell/base16-flat.dark.sh"
 # Vi mode
 bindkey -v
 
-# history
-setopt APPEND_HISTORY
-# for sharing history between zsh processes
-setopt INC_APPEND_HISTORY
-setopt SHARE_HISTORY
-# ignore duplicate history
-setopt HIST_IGNORE_DUPS
-
 # source all config files
 for config_file ($ZSH/*.zsh*); do
     source $config_file
