75ee0e51cbc221c772690e228237a655063723b1
Jonathan Cui <jonathan.cui@hulu.com>
2020-11-20 00:36:00 -0800

    Emacs: Update ivy config. Add counsel-projectile

--- emacs/lisp/init-ivy.el
+++ emacs/lisp/init-ivy.el
@@ -5,14 +5,15 @@
   :delight
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
          :map ivy-switch-buffer-map
-         ("C-l" . ivy-done)
          ("C-d" . ivy-switch-buffer-kill)
-         ("C-k" . ivy-previous-line))
+         ("C-k" . ivy-previous-line)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode +1)
   :custom
@@ -22,8 +23,24 @@
   (ivy-count-format "(%d/%d) "))
 
 (use-package counsel
-  :bind (:map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-f" . counsel-find-file)
+         ("C-x b" . counsel-ibuffer)
+         ("C-M-l" . counsel-imenu)
+         ([remap apropos-command] . counsel-apropos)
+         ([remap describe-variable] . counsel-describe-variable)
+         ([remap describe-symbol] . counsel-describe-symbol)
+         ([remap describe-function] . counsel-describe-function)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (ivy-initial-inputs-alist nil))
+
+(use-package counsel-projectile
+  :requires projectile
+  :demand
+  :config
+  (counsel-projectile-mode 1))
 
 (provide 'init-ivy)
 ;;; init-ivy.el ends here
