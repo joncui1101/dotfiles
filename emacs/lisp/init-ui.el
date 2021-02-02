778cbfd4d90b264042dc4a930f84e813b50a8273
Jonathan Cui <joncui1101@gmail.com>
2021-02-01 22:27:33 -0800

    Emacs: Add smooth scrolling

--- emacs/lisp/init-ui.el
+++ emacs/lisp/init-ui.el
@@ -2,9 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(eval-when-compile
-  (require 'use-package))
-
 (use-package doom-themes
   :config
   (setq doom-themes-enable-bold t
@@ -22,6 +19,15 @@
   (doom-modeline-lsp t)
   (doom-modeline-modal-icon nil)
   (doom-modeline-vcs-max-length 50))
+
+(use-package smooth-scrolling
+  :defer 1
+  :config (smooth-scrolling-mode 1))
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)) ;; one line at a time
+      mouse-wheel-progressive-speed nil ;; don't accelerate scrolling
+      mouse-wheel-follow-mouse 't ;; scroll window under mouse
+      scroll-step 1) ;; keyboard scroll one line at a time
 
 (provide 'init-ui)
 ;;; init-ui.el ends here
