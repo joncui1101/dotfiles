d58c3c7117c53975fbb764a4504f36280e2fa370
Jonathan Cui <joncui1101@gmail.com>
2021-02-28 10:52:32 -0800

    Cleanup flycheck errors

--- emacs/lisp/init-elfeed.el
+++ emacs/lisp/init-elfeed.el
@@ -15,8 +15,8 @@
     "Wrapper to save the elfeed db to disk before burying buffer"
     (interactive)
     (elfeed-db-save)
-    (elfeed-kill-buffer))
-  (defun jc/elfeed-evil-collection-remap (_mode mode-keymaps &rest _rest)
+    (kill-buffer (current-buffer)))
+  (defun jc/elfeed-evil-collection-remap (_mode _mode-keymaps &rest _rest)
     (evil-collection-define-key 'normal 'elfeed-search-mode-map
       (kbd "RET") 'elfeed-search-browse-url
       (kbd "S-<return>") 'elfeed-search-show-entry
--- emacs/lisp/init-functions.el
+++ emacs/lisp/init-functions.el
@@ -1,14 +1,19 @@
+;;; package --- personal functions
+;;; Commentary:
+;;; Code:
 (defun jc/reload-emacs-config ()
+  "Reload Emacs config."
   (interactive)
   (load-file user-init-file))
 
 (defun jc/server-shutdown ()
-  "Save buffers, Quit, and Shutdown (kill) server"
+  "Save buffers, Quit, and Shutdown (kill) server."
   (interactive)
   (save-some-buffers)
   (kill-emacs))
 
 (defun jc/browse-url-firefox (url &optional _new-window)
+  "Open URL in firefox."
   (interactive (browse-url-interactive-arg "URL: "))
   (pcase system-type
     ('darwin (start-process (concat "open " url) nil "open" "-a" "firefox" url))
@@ -16,6 +21,7 @@
     ('gnu/linux (start-process (concat "firefox " url) nil "firefox" url))))
 
 (defun jc/browse-url-vlc (url &optional _new-window)
+  "Open URL in vlc."
   (interactive (browse-url-interactive-arg "URL: "))
   (pcase system-type
     ('darwin (start-process (concat "open " url) nil "open" "-a" "vlc" url "--args" "--video-on-top"))
@@ -23,3 +29,5 @@
     ('gnu/linux (start-process (concat "vlc " url) nil "vlc" url "--video-on-top"))))
 
 (provide 'init-functions)
+;;; init-functions.el ends here
+
